0ee97e184bf372dba16eb99056592ecee768181f
Jan Markus Milan <jmarkusm@me.com>
2020-06-24 18:15:41 -0400

    Added links to different files

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -12,19 +12,19 @@ const HomePageView = () => {
           <div className="header mb-5">
             <h3 className="float-left">Online Portfolios</h3>
             <div className="nav float-right justify-content-center">
-              <a className="nav-link active" href="#">
+              <Link to="/" className="nav-link active">
                 Home
-              </a>
-              <a className="nav-link ml-4" href="#">
+              </Link>
+              <Link to="/portfolios" className="nav-link ml-4">
                 Portfolios
-              </a>
+              </Link>
             </div>
           </div>
         </div>
 
         <div className="overview mt-5">
           <h1>Overview</h1>
-          <p className="lead">
+          <p className="lead mt-4">
             Nowadays, having an online portfolio and presence is really
             important. More importantly, each person has their own preferences
             towards look and design. The purpose of this app is to gather a
@@ -32,7 +32,9 @@ const HomePageView = () => {
             Three portfolios will be made by us, and the rest are free to
             download portfolios made by other people online.
           </p>
-          <button className="btn btn-lg btn-light">Choose A Portfolio!</button>
+          <Link to="/portfolios" className="btn btn-lg btn-light mt-4">
+            Choose A Portfolio!
+          </Link>
         </div>
       </div>
     </>
