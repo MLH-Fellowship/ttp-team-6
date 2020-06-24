efd1ae761a077109384d8d60e37384831b174cf2
Jan Markus Milan <jmarkusm@me.com>
2020-06-24 16:57:07 -0400

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import RoutesContainer from "../components/routes/RoutesContainer";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <RoutesContainer />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
