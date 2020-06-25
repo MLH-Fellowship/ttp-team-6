f2c6eb4714fa42d42160aa7b72e1307e64ab8cbf
Jan Markus Milan <jmarkusm@me.com>
2020-06-25 14:57:41 -0400

    Changed some styling names

--- src/components/views/PortfolioPageView.js
+++ src/components/views/PortfolioPageView.js
@@ -6,65 +6,67 @@ import { Link } from "react-router-dom";
 const PortfolioPageView = () => {
   return (
     <>
-      <ParticlesBg type="circle" bg={true} />
-      <div className="text-center">
-        <div className="navbar-width d-flex h-100 p-3 mx-auto flex-column">
-          <div className="header mb-5">
-            <h3 className="float-left">Online Portfolios</h3>
-            <div className="nav float-right justify-content-center">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-              <Link to="/portfolios" className="nav-link active ml-4">
-                Portfolios
-              </Link>
-              <Link to="/extras" className="nav-link ml-4">
-                Extras
-              </Link>
+      <div className="page-body">
+        <ParticlesBg type="circle" bg={true} />
+        <div className="text-center">
+          <div className="navbar-width d-flex p-3 mx-auto flex-column">
+            <div className="header mb-5">
+              <h3 className="float-left">Online Portfolios</h3>
+              <div className="nav float-right justify-content-center">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+                <Link to="/portfolios" className="nav-link active ml-4">
+                  Portfolios
+                </Link>
+                <Link to="/extras" className="nav-link ml-4">
+                  Extras
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="overview">
-          <h1>Portfolio Page</h1>
-          <p className="lead">
-            This is PortfolioPageView! This page should have a collection of
-            different portfolios, styled using cards from bootstrap.
-          </p>
-        </div>
+          <div className="overview">
+            <h1>Portfolio Page</h1>
+            <p className="lead">
+              This is PortfolioPageView! This page should have a collection of
+              different portfolios, styled using cards from bootstrap.
+            </p>
+          </div>
 
-        <div className="row mt-4 popup">
-          <div className="col">
-            <div className="card">
-              <Link to="/myPortfolio">
-                <img
-                  className="card-img-top portfolio-image"
-                  alt="Image"
-                  src=""
-                />
-              </Link>
+          <div className="row mt-4 popup">
+            <div className="col">
+              <div className="card">
+                <Link to="/myPortfolio">
+                  <img
+                    className="card-img-top portfolio-image"
+                    alt="Image"
+                    src=""
+                  />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="col">
-            <div className="card">
-              <Link to="/">
-                <img
-                  className="card-img-top portfolio-image"
-                  alt="Image"
-                  src=""
-                />
-              </Link>
+            <div className="col">
+              <div className="card">
+                <Link to="/">
+                  <img
+                    className="card-img-top portfolio-image"
+                    alt="Image"
+                    src=""
+                  />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="col">
-            <div className="card">
-              <Link to="/">
-                <img
-                  className="card-img-top portfolio-image"
-                  alt="Image"
-                  src=""
-                />
-              </Link>
+            <div className="col">
+              <div className="card">
+                <Link to="/">
+                  <img
+                    className="card-img-top portfolio-image"
+                    alt="Image"
+                    src=""
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
