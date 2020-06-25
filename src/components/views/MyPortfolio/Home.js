4d9ae27de85423156568204f9f74d833a248316e
Jan Markus Milan <jmarkusm@me.com>
2020-06-25 19:02:25 -0400

    Made the button larger

--- src/components/views/MyPortfolio/Home.js
+++ src/components/views/MyPortfolio/Home.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 const Home = ({ name, description }) => {
   return (
-    <div id="home" className="jumbotron">
+    <div id="home">
       <div class="container">
         <h1 class="home-title" class="load-hidden">
-          Hi, I'm <span class="name-text-color">{name}</span>
+          Hi, I'm <span class="text-info">{name}</span>
         </h1>
         <h4 className="home-description">{description}</h4>
-        <Link to="/" className="btn btn-outline-success mt-2">
-          <strong>Know More</strong>
+        <Link to="/" className="btn btn-lg btn-outline-info mt-2">
+          <strong>Learn More</strong>
         </Link>
       </div>
     </div>
