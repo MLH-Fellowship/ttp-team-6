f06bf6e01ee60eb8be5b216e685a1076f2cfbef8
Lamarr20 <bakerlamarr@gmail.com>
2020-06-25 21:39:31 -0400

    update to about page

--- src/components/views/AboutPageView.js
+++ src/components/views/AboutPageView.js
@@ -6,9 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
+import Media from 'react-bootstrap/Media';
+import Image from 'react-bootstrap/Image';
 const AboutPageView = () => {
   return (
+    
     <>
       <ParticlesBg type="square" bg={true} />
       <div className="text-center">
@@ -35,19 +37,69 @@ const AboutPageView = () => {
  
       </div>
 
-
       <Container>
   <Row>
-    <Col md={4}>
-    <h1> About me</h1>
+    <Col  md={{ span: 4, offset: 4 }}>
+    
+    <h3> About me</h3>
+    <div class="fakeimg">
+    <Image
+     width={128}
+     height={128}
+     className="mr-3"
+    src="https://placehold.it/150x80?text=IMAGE/128x128"
+    alt="Example js"
+  />
+    </div>
     </Col>
-    <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
   </Row>
   <Row>
-    <p>Here is some test about me..</p>
+    <p>Lorem ipsum dolor sit amet, erat justo voluptua per at. Tollit tempor rationibus nam eu, mel ut esse munere albucius, ne qui erat option. Wisi nostrud cum ea, eu tale denique duo. Ei menandri platonem mel, ius prima placerat deseruisse ad. Ad qui petentium signiferumque, ei sale ludus feugait has.</p>
   </Row>
-  <Row>
-    <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
+    <Row>
+    <Col>
+    <Media>
+  <Image
+     width={128}
+     height={128}
+     className="mr-3"
+    src="https://placehold.it/150x80?text=IMAGE/128x128"
+    alt="Example js"
+  />
+
+</Media>
+<h5> Example js</h5>
+</Col>
+
+<Col>
+    <Media>
+  <Image
+     width={128}
+     height={128}
+     className="mr-3"
+    src="https://placehold.it/150x80?text=IMAGE/128x128"
+    alt="Example js"
+    rounded
+  />
+
+</Media>
+<h5> Example js</h5>
+</Col>
+
+<Col>
+    <Media>
+  <Image
+     width={128}
+     height={128}
+     className="mr-3"
+    src="https://placehold.it/150x80?text=IMAGE/128x128"
+    alt="Example js"
+    roundedCircle 
+  />
+
+</Media>
+<h5> Example js</h5>
+</Col>
   </Row>
 </Container>
     </>
